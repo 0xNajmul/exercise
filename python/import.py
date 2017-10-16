09341d377de5826b7334aea595b4a0c89ea707d6
najmul-amin <najmulamin123@gmail.com>
2017-10-16 20:54:38 +0600

    folder

---
+++ python/import.py
@@ -0,0 +1,11 @@
+# example 1
+''' import math
+print(math.pi) '''
+
+# example 2
+''' from math import pi
+print(pi) '''
+
+#example 3
+import sys
+print(sys.path)
---
+++ python/more then one line.py
@@ -0,0 +1,16 @@
+# more then one line
+a=5+6+7+\
+8+9
+print(a)
+
+# example 2
+b=(5+6+7+3+
+5+8+
+2+6)
+print(b)
+
+# example 3
+c=['red',
+    'green',
+    'blue']
+print(c)
