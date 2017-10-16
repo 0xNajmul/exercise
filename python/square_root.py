98e8cec41e329c430dd13d77c026f1133cddab79
najmul-amin <najmulamin123@gmail.com>
2017-10-16 20:44:45 +0600

    square root

---
+++ python/square_root.py
@@ -0,0 +1,30 @@
+# square
+a=5
+print(a**2)
+
+# square root
+b=10
+c=a**.05
+print(c)
+print("the square root is %0.3f"%(c))
+
+x = 15
+y = 4
+
+# Output: x + y = 19
+print('x + y =',x+y)
+
+# Output: x - y = 11
+print('x - y =',x-y)
+
+# Output: x * y = 60
+print('x * y =',x*y)
+
+# Output: x / y = 3.75
+print('x / y =',x/y)
+
+# Output: x // y = 3
+print('x // y =',x//y)
+
+# Output: x ** y = 50625
+print('x ** y =',x**y)
