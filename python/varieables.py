03261b159da24e3f084bf0729f58c89af8c5500b
najmul-amin <najmulamin123@gmail.com>
2017-10-16 20:42:50 +0600

    varieables

---
+++ python/varieables.py
@@ -0,0 +1,20 @@
+# example 1
+a=5
+b=1.23
+c="hello"
+print(a,b,c)
+
+# example 2
+d,e,f = 2,5.23,"hello"
+print(d,e,f)
+
+# example 3
+x=y=z="same"
+
+# finding variable type
+p=5
+q=5.23
+r="hello"
+print(p,"is",type(p),"type of Variable")
+print(q,"is",type(q),"type of Variable")
+print(r,"is",type(r),"type of Variable")
