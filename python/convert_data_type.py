9d2616e15fc5063b777b2a9ee7c58a0c44dab2dc
najmul-amin <najmulamin123@gmail.com>
2017-10-16 20:37:44 +0600

    convert_data_type

---
+++ python/convert_data_type.py
@@ -0,0 +1,20 @@
+# example 1
+x=2.11
+print(int(x))
+
+y=3
+print(float(y))
+
+z=55
+print(str(z))
+
+# example 2
+a="this is test"
+print(list(a))
+
+# example 3
+print('I love {0} and {1}'.format('bread','butter'))
+# Output: I love bread and butter
+
+# example 4
+print("my name is {first} {second}".format(first="najmul",second="amin"))
