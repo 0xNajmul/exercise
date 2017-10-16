5fdd46d3f59d6b7583dcbc3b02abefff5b3b281d
najmul-amin <najmulamin123@gmail.com>
2017-10-16 20:15:59 +0600

    Comments in Python

---
+++ comments.py
@@ -0,0 +1,13 @@
+# this is single line comment
+# this is another Single line comment
+
+#example 2
+'''hello this is another comments'''
+
+#example 3
+"""hello this is another comments
+hello this is another comments
+hello this is another comments
+"""
+
+print ("Comments are working !")
