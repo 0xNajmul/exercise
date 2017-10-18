b9421739e3fb766c03dde2cff021d64bd9cfe6c9
najmul-amin <najmulamin123@gmail.com>
2017-10-18 22:19:15 +0600

    test

--- python/csv.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-with open ('./file.csv','r') as _filehandler:
-    csv_file_reader = csv.DictReader(_filehandler)
-    for row in csv_file_reader:
-        # Do something here
-        print(row['quote'])
-        print(row['speaker'])
