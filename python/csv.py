06dbfe0e95b077a0f4425d24814cf58ffa459e4b
najmul-amin <najmulamin123@gmail.com>
2017-10-18 21:55:52 +0600

    "etc"

--- python/csv.py
+++ python/csv.py
@@ -1,6 +1,6 @@
 import csv
 
-with open('file.csv', 'r') as _filehandler:
+with open ('./file.csv','r') as _filehandler:
     csv_file_reader = csv.DictReader(_filehandler)
     for row in csv_file_reader:
         # Do something here
