876cd3438b75902157e6e954b5ce31ffede9e978
najmul-amin <najmulamin123@gmail.com>
2017-10-19 13:53:46 +0600

    "dict for string"

--- python/dict.py
+++ python/dict.py
@@ -1,5 +1,16 @@
+# example 1
 a={1:'najmul','amin':2}
 print(a)
 print(type(a))
 print(a[1])
 print(a['amin'])
+
+# example 2
+b={
+    "name":"najmul",
+    "age":"twenty two",
+    "city":"Dhaka"
+}
+print(b)
+print(b['age'])
+print(b['name'])
