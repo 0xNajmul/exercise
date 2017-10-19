b20b4d7604fba93664326fce97ad9c7b832080d1
najmul-amin <najmulamin123@gmail.com>
2017-10-19 14:02:47 +0600

    "array to list"

--- python/dict.py
+++ python/dict.py
@@ -14,3 +14,28 @@ b={
 print(b)
 print(b['age'])
 print(b['name'])
+
+
+# example 3
+dictionary = { "some_key": "some_value" }
+
+for key in dictionary:
+    print("%s --> %s" %(key, dictionary[key]))
+# some_key --> some_value   
+
+
+# example 4
+dictionary_tk = {
+  "name": "Leandro",
+  "nickname": "Tk",
+  "nationality": "Brazilian",
+  "age": 24
+}
+
+for attribute, value in dictionary_tk.items():
+    print("My %s is %s" %(attribute, value))
+    
+# My name is Leandro
+# My nickname is Tk
+# My nationality is Brazilian
+# My age is 24
---
+++ python/list.py
@@ -0,0 +1,25 @@
+# example 1
+a=[5,6,7,8,9]
+print(a) 
+
+# example 2
+b=["hello","there"]
+print(b)
+print(b[0])
+
+# example 03
+# if will print c[0] to c[5]
+c=[5,6,7,8,3,9]
+print(c[0:5])
+
+# example 4
+bookshelf = [
+  "The Effective Engineer",
+  "The 4 hours work week",
+  "Zero to One",
+  "Lean Startup",
+  "Hooked"
+]
+
+for book in bookshelf:
+    print(book)
