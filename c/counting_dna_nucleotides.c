353f0ee4db884983088458dc1afab77dc6def2dd
Najmul Amin <najmulamin123@gmail.com>
2017-11-30 23:56:00 +0600

    Update and rename c.txt to counting_dna_nucleotides.c

---
+++ c/counting_dna_nucleotides.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(){
+	int A=0;
+	int C=0;
+	int G=0;
+	int T=0;
+	int i=0;	
+	char inp[100];	
+	printf("Enter Your Input : ");
+	scanf("%s",&inp[i]);
+	int size =strlen(inp);
+	
+	for(i=0;i<size;i++){
+		if(inp[i]=='A'){
+			A++;	
+		}
+		else if(inp[i]=='C'){
+			C++;	
+		}
+		else if(inp[i]=='G'){
+			G++;	
+		}
+		else if(inp[i]=='T'){
+			T++;	
+		}		
+	}	
+	printf("%d %d %d %d",A,C,G,T);	
+}
